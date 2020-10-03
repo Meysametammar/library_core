687a021c6a5349f06bfb71ae3bca3457d8d8df64
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-10-03 11:08:15 +0330

    init books

---
+++ app/Models/Books.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    use HasFactory;
+    const CREATED_AT = 'creation_date';
+    const UPDATED_AT = 'last_update';
+}
